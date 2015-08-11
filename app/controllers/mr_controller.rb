d011e28f1417aba75933e0677690b0d02cdb7e77
Paolo Cosentino <cosepaol88@gmail.com>
2015-08-10 21:36:14 -0500

    added the guys

--- app/controllers/mr_controller.rb
+++ app/controllers/mr_controller.rb
@@ -1,7 +1,41 @@
 class MrController < ApplicationController
-   def bob_frapples
+   def johnny_pezzuto
    end
 
    def pietro_libreri
    end
+
+   def iggy_libreri
+   end
+
+   def claudio_libreri
+   end
+
+   def aldo_libreri
+   end
+
+   def anthony_giovannelli
+   end
+
+   def jerry_giovannelli
+   end
+
+   def joe_guttilla
+   end
+
+   def anthony_bondi
+   end
+
+   def adriano_cosentino
+   end
+
+   def domenico_derango
+   end
+
+   def joey_bruno
+   end
+
+   def joe_lipsey
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root :to => 'pages#index'
   get 'page/index'
-  get 'mr/bob_frapples'
+  get 'mr/johnny_pezzuto'
   get 'mr/pietro_libreri'
+  get 'mr/iggy_libreri'
+  get 'mr/claudio_libreri'
+  get 'mr/aldo_libreri'
+  get 'mr/anthony_giovannelli'
+  get 'mr/jerry_giovannelli'
+  get 'mr/joe_guttilla'
+  get 'mr/anthony_bondi'
+  get 'mr/adriano_cosentino'
+  get 'mr/domenico_derango'
+  get 'mr/joey_bruno'
+  get 'mr/joe_lipsey'
 end
